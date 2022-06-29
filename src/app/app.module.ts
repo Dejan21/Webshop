a7d0c27c295ded0d3987bb598553c8620ddca606
Dejan21 <jangelovski_dejan@yahoo.com>
2022-06-29 22:03:59 +0200

    Api Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CartComponent } from './components/cart/cart.component';
-import { HeaderComponent } from './components/header/header.component';
-import { ProductsComponent } from './components/products/products.component';
+import { StoreComponent } from './components/store/store.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CartComponent,
-    HeaderComponent,
-    ProductsComponent
+    StoreComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
